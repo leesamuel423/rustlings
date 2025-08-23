be8c24834abc2ff043a304baca7079df8c5c487c
Samuel Lee <leesamuel423@gmail.com>
2025-08-23 19:05:07 -0400

    feat: 02_functions

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,5 +1,8 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
 
 fn main() {
+    fn call_me() {
+        print!("hello");
+    }
     call_me(); // Don't change this line
 }
--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -1,9 +1,19 @@
 // TODO: Fix the function body without changing the signature.
 fn square(num: i32) -> i32 {
-    num * num;
+    num * num
 }
 
 fn main() {
     let answer = square(3);
     println!("The square of 3 is {answer}");
 }
+
+/*
+ * semicolon -> expression vs statement
+ * - adding semicolon turns expression (returns a value) to a statement (performs action but
+ * doesn't return anything (technically (), the unit type)
+ *
+ * So general rule:
+ * - Last line of fn that returns a value, no semicolon
+ * - All other lines usually have semicolons b/c they're statements
+ */
